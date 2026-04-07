3438835617b684d2747e51c6282ce2ab6f55de71
liudongyu <10185101162@stu.ecnu.edu.cn>
2026-04-07 17:11:04 +0800

    使用更先进的策略，避免冗长输出

--- src/main/java/com/hexagram2021/dependency_sorter_output/mixin/DependencySorterMixin.java
+++ src/main/java/com/hexagram2021/dependency_sorter_output/mixin/DependencySorterMixin.java
@@ -1,30 +1,39 @@
 package com.hexagram2021.dependency_sorter_output.mixin;
 
 import com.google.common.collect.Multimap;
-import com.hexagram2021.dependency_sorter_output.utils.DSOLogger;
+import com.google.common.collect.Sets;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.util.DependencySorter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 @Mixin(DependencySorter.class)
-public class DependencySorterMixin {
+public class DependencySorterMixin<K, V extends DependencySorter.Entry<K>> {
 	@WrapOperation(method = "lambda$isCyclic$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/DependencySorter;isCyclic(Lcom/google/common/collect/Multimap;Ljava/lang/Object;Ljava/lang/Object;)Z"))
-	private static <K> boolean dependency_sorter_output$isCyclic(Multimap<K, K> dependencies, K source, K target, Operation<Boolean> original) {
-		try {
-			return original.call(dependencies, source, target);
-		} catch (StackOverflowError e) {
-			dependencies.keySet().forEach(key -> DSOLogger.error(
-					"  - " + key + ": " + dependencies.get(key).stream()
-							.filter(Objects::nonNull)
-							.map(Object::toString)
-							.collect(Collectors.joining(", "))
-			));
-			throw new IllegalStateException("Cyclic dependency detected: %s -> %s".formatted(source, target));
+	private static <K> boolean dependency_sorter_output$isCyclic(Multimap<K, K> dependencies, K source, K target, Operation<Boolean> original, @Share(value = "dependency_sorter_output$visiting") LocalRef<Set<K>> visiting) {
+		if(visiting.get() == null) {
+			visiting.set(Sets.newHashSet(source));
 		}
+		if(!visiting.get().add(target)) {
+			throw new IllegalStateException("Cyclic dependency detected: " + visiting.get());
+		}
+		boolean ret = original.call(dependencies, source, target);
+		visiting.get().remove(target);
+		return ret;
+	}
+
+	@WrapOperation(method = "addDependencyIfNotCyclic", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/DependencySorter;isCyclic(Lcom/google/common/collect/Multimap;Ljava/lang/Object;Ljava/lang/Object;)Z"))
+	private static <K> boolean dependency_sorter_output$addDependencyIfNotCyclic(Multimap<K, K> dependencies, K source, K target, Operation<Boolean> original, @Share(value = "dependency_sorter_output$visiting") LocalRef<Set<K>> visiting) {
+		visiting.set(Sets.newHashSet(source));
+		if(!visiting.get().add(target)) {
+			// Self-Dependent
+			throw new IllegalStateException("Cyclic dependency detected: " + visiting.get());
+		}
+		return original.call(dependencies, source, target);
 	}
 }
